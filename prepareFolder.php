0dc75ada5dc4bb0765c173faa33181207d60abfb
Dima <mr.rogovdima@gmail.com>
2017-09-11 23:35:26 +0300

    - strategy pattern complete

--- prepareFolder.php
+++ prepareFolder.php
@@ -8,3 +8,17 @@
 
 //get arguments from cli
 $options = getopt("n:p:");
+
+mkdir("{$options['n']}");
+
+$name = ucfirst($options['n']);
+
+//create README
+$fp = fopen("{$options['n']}/README.md", "w");
+fwrite($fp, "# {$name}\r\n Book: https://www.ozon.ru/context/detail/id/20216992/\r\n Page: {$options['p']} ");
+fclose($fp);
+
+//create run.php
+$fp = fopen("{$options['n']}/run.php", "w");
+fwrite($fp, "<?php \r\n//simple autoloader \r\nrequire_once('../AutoLoader.php'); \r\n\$autoloader = new AutoLoader(); \r\n\r\n//Enter code... \r\n");
+fclose($fp);
