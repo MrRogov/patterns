99b2fb7d6ec221b884bd332265e94c4ce663a913
dima <mr.rogovdima@gmail.com>
2017-10-24 14:50:02 +0300

    -

---
+++ factory/PizzaStore.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rogov
+ * Date: 24.10.17
+ * Time: 14:46
+ */
+
+namespace factory;
+
+
+abstract class PizzaStore
+{
+    public function orderPizza(){
+
+    }
+}
